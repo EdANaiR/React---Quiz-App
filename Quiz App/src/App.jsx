3ba78b91750d8e8fee0aa7a031846794ed22e940
Eda Nur Nair <93126814+EdANaiR@users.noreply.github.com>
2024-08-20 09:33:47 +0300

    Initial commit

---
+++ Quiz App/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let e = "Eda"
+
+  return (
+    <>
+      <div>
+        <p>Kullaıcının Adı : {e}</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
